dc0ad896bbbdc8fdfaeba7ded62a8091448b04d1
Nathan Bartyuk <nathanb1357@gmail.com>
2023-06-10 20:45:04 -0700

    Set up mysql POST and GET

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [bookName, setBookName] = useState('');
+  const [bookReview, setBookReview] = useState('');
+  const [reviewList, setReviewList] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
+      console.log(response.data)
+      setReviewList(response.data);
+    })
+  }, [])
+
+  const submitCheck = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      bookName: bookName, 
+      bookReview: bookReview
+    }).then(() => {
+      alert("Succesful insert");
+    });
+  };
+
   return (
     <div className="App">
       <h1>Book Reviews</h1>
       <div className='Form'>
         <label>Book Title:</label>
-        <input type='text' name='bookTitle'/>
-        <label>Review:</label>
-        <input type='text' name='bookReview'/>
+        <input type='text' name='bookTitle' onChange={(e) => {setBookName(e.target.value)}}/>
 
-        <button>Submit</button>
+        <label>Review:</label>
+        <input type='text' name='bookReview' onChange={(e) => {setBookReview(e.target.value)}}/>
+
+        <button onClick={submitCheck}>Submit</button>
       </div>
+      {reviewList.map((val) => {
+        return ( 
+          <div key={val.br_id}>
+            <h2>Book: {val.br_name}</h2>
+            <p>Review: {val.br_review}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-    res.send("Hello page!")
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "reactpractice",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM book_reviews";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });    
 })
 
+app.post('/api/insert', (req, res) => {
+    const bookName = req.body.bookName;
+    const bookReview = req.body.bookReview;
+    const sqlInsert = "INSERT INTO book_reviews (br_name, br_review) VALUES (?,?)";
+    db.query(sqlInsert, [bookName, bookReview], (err, result) => {
+        console.log(err);
+    });
+});
+
 app.listen(3001, () => {
-    console.log("Running on port 3001")
+    console.log("Running on port 3001");
 });
